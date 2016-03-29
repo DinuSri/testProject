6d25c399d77ac2bd936c31c42bd298e964291d2a
DinuSri <flytodinu@gmail.com>
2016-03-29 15:41:28 +0530

    resized form

--- testgit/Form1.Designer.cs
+++ testgit/Form1.Designer.cs
@@ -34,28 +34,28 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 36);
+            this.button1.Location = new System.Drawing.Point(60, 173);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(152, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "New Form Open";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(167, 36);
+            this.button2.Location = new System.Drawing.Point(295, 173);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(153, 44);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Empty";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(512, 280);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
