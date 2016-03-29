fb443d3d60352032a42262df45340ea740436466
nudigraphics <nudigraphics@gmail.com>
2016-03-29 15:32:01 +0530

    form added

--- testgit/t2.cs
+++ testgit/t2.cs
@@ -9,17 +9,11 @@ using System.Windows.Forms;
 
 namespace testgit
 {
-    public partial class Form1 : Form
+    public partial class t2 : Form
     {
-        public Form1()
+        public t2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-             t2 t2 = new t2();
-            t2.ShowDialog();   
         }
     }
 }
